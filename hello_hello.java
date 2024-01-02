5cdd229ed7b5d6ab0f374b68e2f3b605b464e43e
ajazlodhi <150616811+ajazlodhi@users.noreply.github.com>
2024-01-02 11:15:23 -0500

    Update hello_hello.java

--- hello_hello.java
+++ hello_hello.java
@@ -1,5 +1,13 @@
-public class hello_hello {
+public class MySwitchStatementTwo{
     public static void main(String[] args) {
-        System.out.println("Hello Hello");
+        int number = 44;
+        String size = switch (number){
+            case 29 -> "Small";
+            case 42 -> "Medium";
+            case 44 -> "Large";
+            case 48, 49, 50 -> "Extra Large";
+            default -> "UNKNOWN";
+        };
+        System.out.println("Size : " + size);
     }
 }
